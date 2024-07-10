be55de2a754baecba35f416bf2b5c893d6371f0a
GiuliaPeretti <giulia.peretti12@gmail.com>
2024-07-10 18:28:00 +0200

    problema con state

--- app/src/main/java/com/example/android_calculator/MainActivity.kt
+++ app/src/main/java/com/example/android_calculator/MainActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.android_calculator.ui.theme.Android_calculatorTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +25,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             Android_calculatorTheme {
                 val viewModel = viewModels<CalculatorViewModel>()
+                val state = viewModel.state
                 val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    onAction = viewModel::onAction,
+                    buttonSpacing = buttonSpacing,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Black)
+                        .padding(16.dp)
+                )
              }
         }
     }
